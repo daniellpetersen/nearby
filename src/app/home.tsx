ce955f86a5066a7af86d2d6c193ac929dfefb006
Daniel Petersen <danielpetersen@superahost.com>
2025-03-27 16:51:24 -0400

    fix: botões das categorias

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { View, Text, Alert } from "react-native"
+import { View, Alert } from "react-native"
 
 import { api } from "@/services/api"
 
@@ -7,12 +7,14 @@ import { Categories, CategoriesProps } from "@/components/categories"
 
 export default function Home(){
     const [categories, setCategories] = useState<CategoriesProps>([])
+    const [category, setCategory] = useState("")
 
     async function fetchCategories()
     {
         try{
             const { data } = await api.get("/categories")
             setCategories(data)
+            setCategory(data[0].id)
         } catch (error) {
             console.log(error)
             Alert.alert("Categorias", "Não foi possível carregar as categorias.")
@@ -25,7 +27,11 @@ export default function Home(){
 
     return (
         <View style={{ flex: 1 }}>
-            <Categories data={categories} />
+            <Categories 
+                data={categories} 
+                onSelect={setCategory} 
+                selected={category} 
+            />
         </View>
     )
 }
